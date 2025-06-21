e5768c901d552ea3a0a1b1f93e60edcdfe7c7d31
carlhuxley <carlhuxley@gmail.com>
2025-06-21 06:46:47 +0000

    configure db

--- config.py
+++ config.py
@@ -2,9 +2,13 @@
 import os
 from pydantic_settings import BaseSettings
 
+db_user = os.environ.get("POSTGRES_USER")
+db_password = os.environ.get("POSTGRES_PASSWORD")
+db = os.environ.get("POSTGRES_DB")
+
 class Settings(BaseSettings):
     # Database settings
-    postgres_url: str = "postgresql://user:password@localhost/apprentice_hub"
+    postgres_url: str = "postgresql://{db_user}:{db_password}@localhost/{db}"
     mongodb_url: str = "mongodb://localhost:27017/"
     
     # Security settings
